ecf39db90bb2948b7d0dbbeeca65ce57ade6a7e8
clevessonmendonca <wanderbloks311@gmail.com>
2023-02-23 16:08:22 -0300

    feat: learning about Image

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout className={roboto.className}>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import { Poppins } from "@next/font/google";
+import Image from 'next/image'
+
+import profilePic from '@/public/images/download.png'
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+
+      <Image src={profilePic} width={200} height={200} alt="Camera photo" />
     </div>
   );
 }
