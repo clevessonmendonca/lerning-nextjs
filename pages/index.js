9d890f2971a36165f02957098058892ced066cd6
clevessonmendonca <wanderbloks311@gmail.com>
2023-02-23 15:25:33 -0300

    feat: learning about Component in Next

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
+import Header from "@/components/Header";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -8,12 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Home</h1>
       <ul>
         <li>
-          <Link href="/products">
-            Products
-          </Link>
+          <Link href="/products">Products</Link>
         </li>
       </ul>
     </div>
